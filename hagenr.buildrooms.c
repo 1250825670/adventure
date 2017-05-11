ac9169431637bead0499090654a5601f68df9963
ironic <ronjhagen@gmail.com>
2017-05-11 06:53:13 -0500

    fix comment typo in buildrooms

--- hagenr.buildrooms.c
+++ hagenr.buildrooms.c
@@ -2,7 +2,7 @@
  * author: ron hagen, cs 344-400
  * program2: hagenr.buildrooms.c
  * description: creates an array of 7 room struts with randomly assigned names,
- * randomly assigned room types, and a randomly number of inter-room
+ * randomly assigned room types, and a random number of inter-room
  * connections. a new directory is created with the prefix "hagenr.rooms." and
  * a suffix of the PID. then each struct is written to a separate file. only 
  * low-level file functions are used.
